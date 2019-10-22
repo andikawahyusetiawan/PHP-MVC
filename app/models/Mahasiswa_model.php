d6e350eb257ca5aa8e8d18695201b63ee18774d6
Andika Wahyu Setiawan <andikawahyusetiawan@mail.unpas.ac.id>
2019-10-23 01:57:04 +0700

    menambahkan database

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,50 +1,25 @@
 <?php
 
 class Mahasiswa_model {
-	// private $mhs = [
-	// 	[
-	// 		"nama" => "Andika Wahyu Setiawan",
-	// 		"nrp" => "173040021",
-	// 		"email" => "andikawahyusetiawan@mail.unpas.ac.id",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	],
-
-	// 	[
-	// 		"nama" => "Andika wahyu",
-	// 		"nrp" => "1730400211",
-	// 		"email" => "andikawahyusetiawan1@mail.unpas.ac.id",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	],
-
-	// 	[
-	// 		"nama" => "Wahyu Setiawan",
-	// 		"nrp" => "1730400212",
-	// 		"email" => "andikawahyusetiawan3@mail.unpas.ac.id",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	]
-	// ];
-
-	private $dbh;
-	private $stmt;
+	private $table = 'mahasiswa';
+	private $db;
 
 	public function __construct()
 	{
-		$dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-		try
-		{
-			$this->dbh = new PDO($dsn, 'root', '');
-		} catch(PDOException $e) {
-			die($e->getMssage);
-		}
+		$this->db = new Database;
 	}
 
 	public function getAllMahasiswa()
 	{
-		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-		$this->stmt->execute();
-		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+		$this->db->query('SELECT * FROM ' . $this->table);
+		return $this->db->resultSet();
+	}
 
-}
+	public function getMahasiswaById($id)
+	{
+		$this->db->query('SELECT * FROM ' .  $this->table . 'WHERE id =:id');
+		$this->db->bind('id', $id);
+		return $this->db->resultSet();
+	}
 
 }
