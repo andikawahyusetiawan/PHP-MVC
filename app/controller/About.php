daec96d15ff117677282d5d7eca0b20b71d8fa2c
Andika Wahyu Setiawan <andikawahyusetiawan@mail.unpas.ac.id>
2019-10-22 22:05:31 +0700

    menambahkan views

--- app/controller/About.php
+++ app/controller/About.php
@@ -1,11 +1,23 @@
 <?php
 
-class About {
-	public function index($nama = 'wahyu', $pekerjaan = 'mahasiswa'){
-		echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+class About extends Controller {
+
+	public function index($nama = 'wahyu', $umur = 32, $pekerjaan = 'mahasiswa')
+	{
+		$data['nama'] = $nama;
+		$data['umur'] = $umur;
+		$data['pekerjaan'] = $pekerjaan;
+		$data['judul'] = 'About Me';
+		$this->view('templates/header', $data);
+		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
 
-	public function page(){
-		echo 'About/page';
+	public function page()
+	{
+		$data['judul'] = 'Pages';
+		$this->view('templates/header', $data);
+		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
